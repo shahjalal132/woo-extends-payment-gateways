e9948f3ea9da86b11a0b5aae59d09258bc69ba63
shahjalal132 <rjshahjalal132@gmail.com>
2024-06-24 16:50:55 +0600

    added btc for nowpayments

--- nowpayments-for-woocommerce.php
+++ nowpayments-for-woocommerce.php
@@ -75,6 +75,7 @@ if ( !defined( 'NPWC_PLUGIN_DIR_PATH' ) ) {
 }
 
 require dirname( NPWC_PLUGIN_FILE ) . '/includes/class-npwc-init.php';
+require dirname( NPWC_PLUGIN_FILE ) . '/includes/class-btc.php';
 
 add_action( 'plugins_loaded', 'load_npwc' );
 
